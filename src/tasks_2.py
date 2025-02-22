46722420212f2452ebf8b2bd7f331768b8d503e4
melumyan-araik <you@example.com>
2025-02-22 13:26:50 +0300

    Убрать из аутентификации проверку политик

--- src/tasks_2.py
+++ src/tasks_2.py
@@ -32,28 +32,18 @@ def calculate_md5_hash(data: str) -> str:
     md5_hash = hashlib.md5(encoded_data).hexdigest()
     return md5_hash
 
-def authentication(real_password: str, candidate_password: str, password_policy: str) -> bool:
+def authentication(real_password: str, candidate_password: str) -> bool:
     """
     Аутентифицирует пользователя, сравнивая MD5-хеш предложенного пароля (candidate_password)
-    с MD5-хешем реального пароля (real_password), предварительно убедившись, что оба пароля
-    соответствуют заданной политике паролей.
+    с MD5-хешем реального пароля (real_password).
 
     Args:
         real_password: Реальный, сохраненный пароль (используется как эталон).
         candidate_password: Пароль, предоставленный пользователем для аутентификации.
-        password_policy: Строка регулярного выражения, определяющая политику паролей.
 
     Returns:
         True, если предложенный пароль совпадает с реальным (на основе сравнения их MD5-хешей)
         и оба пароля соответствуют заданной политике; False в противном случае.
-
-    Raises:
-        AssertionError: Если реальный или предложенный пароль не соответствуют заданной политике.
-                         Это указывает на ошибку программирования или попытку обойти валидацию
-                         политики паролей.
     """
-    assert check_password_policy(real_password, password_policy), f"real_password не соответствует заданной политике {password_policy}"
-    assert check_password_policy(candidate_password, password_policy), f"candidate_password не соответствует заданной политике {password_policy}"
-
     return calculate_md5_hash(real_password) == calculate_md5_hash(candidate_password)
 
